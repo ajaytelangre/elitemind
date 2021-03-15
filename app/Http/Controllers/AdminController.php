669dbbfff1659d6bcae49ed0f86e409433aee4e9
ajaytelangre <ajay@neuleaflifespace.com>
2021-03-15 14:22:09 +0530

    register user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Register;
 use Auth;
+use Validator;
+use Session;
+use Redirect;
 
 class AdminController extends Controller
 {
@@ -18,5 +21,55 @@ class AdminController extends Controller
         $data['users']=Register::get();
         return view("showusers",$data);
 
+    }
+
+    public function register_user(Request $request){
+      //  return $request->all();
+
+        $validatedData=Validator::make($request->all(),[
+            "name"=>"required",
+            "mobile"=>"required|unique:register,phone|max:10|min:10",
+            "email"=>"email|required|unique:register",
+            "password"=>"required",
+            "confirm_password"=>"required",
+            "address"=>"required",
+            "dob"=>"required",
+            "profession"=>"required",
+            "qualification"=>"required",
+        ]);
+
+        if($validatedData->fails())
+        {
+           
+            return Redirect::back()->withErrors($validatedData);
+        }
+        else{
+            
+            $password=$request->password;
+            $confirm_pass=$request->confirm_password;
+            if($password==$confirm_pass)
+            {
+                $register=new Register;
+                $register->username=$request->name;
+                $register->phone=$request->mobile;
+                $register->password=base64_encode($request->password);
+                $register->confirm_password=base64_encode($request->confirm_password);
+                $register->address=$request->address;
+                $register->dob=$request->dob;
+                $register->profession=$request->profession;
+                $register->graduation=$request->qualification;
+                $register->email=$request->email;
+                $register->save();
+                return Redirect::back()->with("success","Registration done");
+
+
+            }
+            else{
+                return Redirect::back()->with("message","Both Password does not match");
+            }
+
+
+        }
+        return $request->all();
     }
 }
